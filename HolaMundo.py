0733cb88fd3c18b1c28561eb2d6ad49e42e1273b
Pepe <jjcamarena@gmail.com>
2024-03-22 01:28:27 -0600

    Ejemplos print

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,22 @@
 print("¡Hola, Mundo!")
+
+print("La Witsi Witsi Araña subió a su telaraña.")
+print()
+print("Vino la lluvia y se la llevó.")
+
+print()
+print()
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+print()
+
+print("La Witsi Witsi Araña" , "subió" , "a su telaraña.")
+
+print()
+print()
+
+print("Mi nombre es", "Python.")
+print("Monty Python.")
